1809610cd27f9d8b93908847c5d7da0d025c718c
Єгембердінов Антон <118931658+KarazingaBazinga@users.noreply.github.com>
2024-06-01 00:40:11 +0300

    Revert "Add display form to update user description"
    This reverts commit 4cce4a7b5695cdfa56f2455ada43e008090a3338.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,8 +2,4 @@ class MembersController < ApplicationController
     def show
       @user = User.find(params[:id])
     end
-    
-  def edit_description
-    @user = User.find(params[:id])
-  end
   end
