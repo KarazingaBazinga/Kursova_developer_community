cbb75c5383affc4d561c5034bc1731392194dff3
Oleg <olegkucherenko12@gmail.com>
2024-06-02 17:47:34 +0300

    Added work experience specs

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,49 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "Logins", type: :feature do
+RSpec.feature 'Logins', type: :feature do
   shared_examples 'a successful login' do |login_field|
     let(:user) { create(:user) }
-         visit root_path
-
-      fill_in 'user_login', with: user.send(login_field)
-      fill_in 'user_password', with: user.password
-
-      click_button 'Log in'
-      expect(page).to have_link('Dev Community')
-      expect(page).to have_link('My Profile')
-      expect(page).to have_link('My Network')
-      expect(page).to have_link('Sign Out')
-      expect(page).to have_text('Search professionals across the world!')
-      expect(page).to have_text(user.name)
-      expect(page).to have_text(user.profile_title)
-    end
-
-    it 'should login user with email' do
-      user = User.create(
-               email: 'user@example.com',
-               password: 'password',
-               username: 'example_user',
-               first_name: 'Example',
-               last_name: 'User',
-               profile_title: 'Senior ruby on rails developer',
-               confirmed_at: DateTime.now
-             )
-
+    it "should login user with #{login_field}" do
       visit root_path
-
-      fill_in 'user_login', with: user.email
+      fill_in 'user_email', with: user.email
       fill_in 'user_password', with: user.password
-
-      click_button 'Log in'
+      click_button 'Log In'
       expect(page).to have_link('Dev Community')
-      expect(page).to have_link('My Profile')
+      expect(page).to have_text('Signed in successfully.')
       expect(page).to have_link('My Network')
       expect(page).to have_link('Sign Out')
-      expect(page).to have_text('Search professionals across the world!')
       expect(page).to have_text(user.name)
       expect(page).to have_text(user.profile_title)
     end
   end
+
   describe 'login' do
     include_examples 'a successful login', :username
     include_examples 'a successful login', :email
