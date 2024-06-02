34c464cb038fa6fe52e0ec2dc0050c084f3c6bcd
Oleg <olegkucherenko12@gmail.com>
2024-06-02 20:59:58 +0300

    Revert "Added model specs and test coverage"
    This reverts commit 74ef83cb62b6cbe8d567d68a29e7253a0242d8a9.

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 class WorkExperience < ApplicationRecord
-  EMPLOYMENT_TYPE = %w[Full-time Part-time Self-employeed Freelance Trainee Internship].freeze
-  LOCATION_TYPE = %w[On-site Hybrid Remote].freeze
+
+  EMPLOYMENT_TYPE = ['Full-time', 'Part-time', 'Self-employeed', 'Freelance', 'Trainee', 'Internship']
+  LOCATION_TYPE = ['On-site', 'Hybrid', 'Remote']
   belongs_to :user
 
   validates :company, :start_date, :job_title, :location, presence: true
@@ -14,15 +13,15 @@ class WorkExperience < ApplicationRecord
   validate :end_date_greater_than_start_date, if: :currently_not_working_here?
 
   def work_experience_last_date
-    return unless end_date.present? && currently_working_here
-
-    errors.add(:end_date, ' must be blank if you are currently working in this company')
+    if end_date.present? && currently_working_here
+      errors.add(:end_date, ' must be blank if you are currently working in this company')
+    end
   end
 
   def presence_of_end_date
-    return unless end_date.nil? && !currently_working_here
-
-    errors.add(:end_date, ' must be present if you are not currently working in this company')
+    if end_date.nil? && !currently_working_here
+      errors.add(:end_date, ' must be present if you are not currently working in this company')
+    end
   end
 
   def currently_not_working_here?
@@ -30,11 +29,11 @@ class WorkExperience < ApplicationRecord
   end
 
   def end_date_greater_than_start_date
-    return if end_date.nil? || start_date.nil?
+    return if end_date.nil?
 
-    return unless end_date < start_date
-
-    errors.add(:end_date, ' must be greater than start_date')
+    if end_date < start_date
+      errors.add(:end_date, ' must be greater than start_date')
+    end
   end
 
   def company_with_employment_type
@@ -54,11 +53,12 @@ class WorkExperience < ApplicationRecord
 
     result = months.divmod(12)
 
-    duration = "#{result.first} #{result.first > 1 ? 'years' : 'year'} #{result.last} #{result.last > 1 ? 'months' : 'month'}"
+    duration = "#{result.first} #{result.first > 1 ? 'years' : 'year'} #{result.last} #{result.last > 1 ? 'months' : 'month' }"
+
     if currently_working_here
-      "#{start_date.strftime('%b %Y')} - Present (#{duration})"
+      "#{start_date.strftime("%b %Y")} - Present (#{duration})"
     else
-      "#{start_date.strftime('%b %Y')} - #{end_date.strftime('%b %Y')} (#{duration})"
+      "#{start_date.strftime("%b %Y")} - #{end_date.strftime("%b %Y")} (#{duration})"
     end
   end
 
