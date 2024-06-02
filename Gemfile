21c7fd6bb1a8d67dd552d3e29c8b5aea466048b0
Oleg <olegkucherenko12@gmail.com>
2024-06-02 21:08:31 +0300

    Revert "maximized chrome and stimulating the user login"
    This reverts commit 8a7e09ce8b04982604b55c7198f2f60df201354a.

--- Gemfile
+++ Gemfile
@@ -48,8 +48,6 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
   gem "letter_opener"
-  gem "factory_bot_rails"
-  gem "rspec-rails", '~> 6.0.0'
 end
 
 group :development do
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 end
